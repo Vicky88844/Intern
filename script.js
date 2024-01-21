a5c49ce5e0d1ab4069f21f3f4143e72f812b7b08
Vicky88844 <vikneshrs555@gmail.com>
2024-01-21 16:04:55 +0530

    image resizing

--- script.js
+++ script.js
@@ -1,21 +1,91 @@
-const repositoryContainer = document.getElementById('repository-container');
+const APIURL = 'https://api.github.com/users/' 
 
-// Replace 'username' with the GitHub username you want to fetch repositories for
-const username = 'username';
-const apiUrl = `https://api.github.com/users/${username}/repos`;
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
 
-fetch(apiUrl)
-	.then(response => response.json())
-	.then(data => {
-		data.forEach(repo => {
-			const repository = document.createElement('div');
-			repository.className = 'repository';
-			repository.innerHTML = `
-				<h3><a href="${repo.html_url}" target="_blank">${repo.name}</a></h3>
-				<p>Description: ${repo.description || '(No description provided)'}</p>
-				<p>Stars: ${repo.stargazers_count}</p>
-				<p>Forks: ${repo.forks_count}</p>
-			`;
-			repositoryContainer.appendChild(repository);
-		});
-	});
+async function getUser(username){
+    try{
+       const {data} = await axios(APIURL + username)
+       createUserCard(data);
+       getRepos(username)
+    }catch(err){
+        if(err.response.status == 404){
+            createErrorCard('No profile with this username')
+        }
+      
+    }
+}
+
+async function getRepos(username){
+    try{
+        console.log(username)
+        const {data} = await axios(APIURL + username + '/repos?sort=created')
+        addReposToCard(data);
+        
+     }catch(err){
+         
+        createErrorCard('Problem fetching repos')
+         
+       
+     }
+}
+
+function createUserCard(user){
+    const cardHTML = `<div class="card">
+    <div>
+        <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+        <ul>
+            <li>${user.followers} <strong>Followers</strong></li>
+            <li>${user.following} <strong>Following</strong></li>
+            <li>${user.public_repos} <strong>Repos</strong></li>
+        </ul>
+
+        <div id="repos">
+        
+        </div>
+
+    </div>
+</div>`
+
+   main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg){
+    const cardHTML = `
+    <div class="card">
+    <h1>${msg}</h1>
+    </div>
+    `
+
+    main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos){
+    const reposEl = document.getElementById('repos');
+    console.log(reposEl)
+    repos
+    .slice(0,10)
+    .forEach(repo => {
+        const repoEl = document.createElement('a')
+        repoEl.classList.add('repo')
+        repoEl.href = repo.html_url
+        repoEl.target = '_blank'
+        repoEl.innerText = repo.name
+        reposEl.appendChild(repoEl);
+    })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+    if(user){
+      getUser(user)
+      search.value = ''
+    }
+})
